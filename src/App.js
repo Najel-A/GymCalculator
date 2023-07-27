f356d6f393fa9aeb8f3ce6d6a063a2a958f6361a
Najel Alarcon <najelalarcon@gmail.com>
2023-07-26 21:22:03 -0700

    Finish Basic calculation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, button} from 'react';
 
 function App() {
+  const [weightInputText, setWeightText] = useState('');
+  const [repsInputText, setRepsText] = useState('');
+  const [oneRepMax, setOneRepMax] = useState('');
+
+  const handleWeightChange = weight => {
+    setWeightText(weight.target.value);
+  }
+
+  const handleRepsChange = reps => {
+    setRepsText(reps.target.value);
+  }
+
+  const handleCalculateClick = () => {
+    const weight = parseFloat(weightInputText);
+    const reps = parseFloat(repsInputText);
+
+    if (!isNaN(weight) && !isNaN(reps) && reps !== 0) {
+      const calculatedOneRepMax = weight / ((1.0278) - (0.0278 * reps));
+      setOneRepMax(calculatedOneRepMax.toFixed(1));
+    } else {
+      setOneRepMax();
+    }
+  };
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi</h1>
+      <label>
+        Weight:
+        <input
+          type="text"
+          value={weightInputText}
+          onChange={handleWeightChange}
+        />
+      </label>
+
+      <label>
+        Reps:
+        <input
+          type="text"
+          value={repsInputText}
+          onChange={handleRepsChange}
+        />
+      </label>
+
+      <button onClick={handleCalculateClick}>Calculate</button>
+      <label>
+        1 Rep Max:
+        <input
+          type="text"
+          value={oneRepMax}
+          readOnly
+        />
+      </label>
     </div>
   );
 }
